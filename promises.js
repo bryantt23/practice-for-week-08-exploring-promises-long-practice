f41d95ae0c6badf6ed834d5a492a6d59f344bc4a
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-08 03:03:16 -0700

    phase 4

--- promises.js
+++ promises.js
@@ -25,11 +25,26 @@ waiting();
 /* --------------------- creating a custom Promise --------------------- */
 
 // Your code here
+async function waitForMyPromise() {
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve('done!!!');
+    }, 1000);
+  });
 
+  const result = await promise;
+  console.log('my promise is', result);
+}
+waitForMyPromise();
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
 
 // Your code here
+new Promise(resolve => {
+  setTimeout(() => {
+    resolve('done!');
+  }, 1500);
+}).then(r => console.log('then my other promise is', r));
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
